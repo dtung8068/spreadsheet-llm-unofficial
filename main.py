4a3d254b0688246155a89eb769c0761dd84edbe0
Darren Tung <56087882+dtung8068@users.noreply.github.com>
2024-08-06 12:03:14 -0400

    compress_spreadsheet reads one at a time

--- main.py
+++ main.py
@@ -2,49 +2,55 @@ import os
 import pandas as pd
 import xlrd
 
+from openai import OpenAI
+
 from SheetCompressor import SheetCompressor
+from SpreadsheetLLM import SpreadsheetLLM
 
 DIRECTORY = 'VFUSE'   
 
-def main():
-    for root, dirs, files in os.walk(DIRECTORY):
-        for file in files:
-            sheet_compressor = SheetCompressor()
-            if file == 'readme.txt':
-                continue
-            try:
-                wb = xlrd.open_workbook(os.path.join(root, file), logfile=open(os.devnull,'w'), formatting_info=True)
-            except xlrd.biffh.XLRDError:
-                return 
-            sheet = pd.read_excel(wb, engine='xlrd')
-            sheet = sheet.apply(lambda x: x.str.replace('\n', '<br>') if x.dtype == 'object' else x)
+def compress_spreadsheet(file):
+    sheet_compressor = SheetCompressor()
+    if file == 'readme.txt':
+        return
+    try:
+        wb = xlrd.open_workbook(os.path.join(root, file), logfile=open(os.devnull,'w'), formatting_info=True)
+    except xlrd.biffh.XLRDError:
+        return 
+    sheet = pd.read_excel(wb, engine='xlrd')
+    sheet = sheet.apply(lambda x: x.str.replace('\n', '<br>') if x.dtype == 'object' else x)
 
-            #Move columns to row 1
-            sheet.loc[-1] = sheet.columns
-            sheet.index += 1
-            sheet.sort_index(inplace=True)
-            sheet.columns = list(range(len(sheet.columns)))
+    #Move columns to row 1
+    sheet.loc[-1] = sheet.columns
+    sheet.index += 1
+    sheet.sort_index(inplace=True)
+    sheet.columns = list(range(len(sheet.columns)))
 
-            #Structural-anchor-based Extraction
-            sheet = sheet_compressor.anchor(sheet)
+    #Structural-anchor-based Extraction
+    sheet = sheet_compressor.anchor(sheet)
 
-            #Encoding 
-            markdown = sheet_compressor.encode(wb, sheet) #Paper encodes first then anchors; I chose to do this in reverse
+    #Encoding 
+    markdown = sheet_compressor.encode(wb, sheet) #Paper encodes first then anchors; I chose to do this in reverse
 
-            #Data-Format Aggregation
-            markdown['Category'] = markdown['Value'].apply(lambda x: sheet_compressor.category(x))
-            category_dict = sheet_compressor.inverted_category(markdown) 
-            try:
-                areas = sheet_compressor.identical_cell_aggregation(sheet, category_dict)
-            except RecursionError:
-                continue
+    #Data-Format Aggregation
+    markdown['Category'] = markdown['Value'].apply(lambda x: sheet_compressor.category(x))
+    category_dict = sheet_compressor.inverted_category(markdown) 
+    try:
+        areas = sheet_compressor.identical_cell_aggregation(sheet, category_dict)
+    except RecursionError:
+        return
 
-            #Inverted-index Translation
-            compress_dict = sheet_compressor.inverted_index(markdown)
+    #Inverted-index Translation
+    compress_dict = sheet_compressor.inverted_index(markdown)
 
-            #Write to files
-            sheet_compressor.write_areas('output/' + file.split('.')[0] + '_areas.txt', areas)
-            sheet_compressor.write_dict('output/' + file.split('.')[0] + '_dict.txt', compress_dict)
+    #Write to files
+    sheet_compressor.write_areas('output/' + file.split('.')[0] + '_areas.txt', areas)
+    sheet_compressor.write_dict('output/' + file.split('.')[0] + '_dict.txt', compress_dict)
+
+def llm(model):
+    spreadsheet_llm = SpreadsheetLLM(model)
 
 if __name__ == "__main__":
-    main()
+    for root, dirs, files in os.walk(DIRECTORY):
+        for file in files:
+            compress_spreadsheet(file)
